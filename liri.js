31d3dcbfecb9280b6eed8d4291ed651385eb9727
Bradley Pagano <bpagano5243@gmail.com>
2019-11-11 21:29:00 -0500

    adding working seatgeek search

--- liri.js
+++ liri.js
@@ -1,14 +1,15 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+//require("dotenv").config();
+//var keys = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
-var moment = require("moment");
+//var moment = require("moment");
 var searchType = process.argv[2];
-var searchTerm = process.argv[3];
 
 switch (searchType){
     case "concert-this":
+        searchTerm = process.argv.slice(3).join("-");
         console.log("concert");
+        getConcert();
         break;
     case "spotify-this-song":
         console.log("spotify");
@@ -17,7 +18,19 @@ switch (searchType){
         console.log("movie");
         break;
     case "do-what-it-says":
-        console.log("Oh LAWD!!! Do What he Say!!!");
+        console.log("Oh LAWD!!! Do What He Say!!!");
         break;
 }
 
+function getConcert() {
+    axios.get("https://api.seatgeek.com/2/events?client_id=OTA1MzgzM3wxNTcyNTMzMTQ3Ljk3&performers.slug=" + searchTerm)
+    .then(function(response){
+        for(i=0; i < response.data.events.length; i++){
+            console.log(response.data.events[i].venue.name);
+            console.log(response.data.events[i].venue.display_location);
+            console.log(response.data.events[i].datetime_local);
+            console.log("--------------------");
+        }
+        
+    })
+}
