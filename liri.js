83026605ed2d55735fee6f8de7023c65090dc06a
Bradley Pagano <bpagano5243@gmail.com>
2019-11-11 20:50:14 -0500

    adds switch case with console logs

--- liri.js
+++ liri.js
@@ -1,6 +1,23 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var searchType = process.argv[2];
+var searchTerm = process.argv[3];
 
-
+switch (searchType){
+    case "concert-this":
+        console.log("concert");
+        break;
+    case "spotify-this-song":
+        console.log("spotify");
+        break;
+    case "movie-this":
+        console.log("movie");
+        break;
+    case "do-what-it-says":
+        console.log("Oh LAWD!!! Do What he Say!!!");
+        break;
+}
 
